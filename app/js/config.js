51b9f684e9b470f695293bb8c5fcf7635a9cb9de
Regency Business Solutions <GitHub@regencyop.com>
2016-09-23 17:07:05 -0400

    Update config.js

--- app/js/config.js
+++ app/js/config.js
@@ -12,25 +12,13 @@ four51.app.config(['$provide', function($provide) {
 				        trackJs.error("API: " + JSON.stringify(ex));
 			        }
 			        catch (x) {
-				        if ((JSON.stringify(ex).indexOf("Inactive Product")) && (document.getElementsByClassName("errorLogContainterEmpty").length !== 0)) {
-//				            $( "<p>Test</p>" ).insertAfter( ".inner" );
-
-				            document.getElementsByClassName("errorLogContainterEmpty")[0].className = "errorLogContainterError";
-				            var newErrorMessage = document.createElement('p');
-                            newErrorMessage.appendChild(document.createTextNode('This size currently out of stock.'));
-                            document.getElementsByClassName("errorLogContainterError")[0].appendChild(newErrorMessage);
-                            //update here
-				        }
-                        if (document.getElementsByClassName("errorLogContainterEmpty").length === 0) {
-			                console.log("does not exist");
+			           
+			         if ((JSON.stringify(ex).indexOf("Inactive Product")) && (document.getElementById("451_btn_orderadd").length !== 0)) {
 			                $( "<div class='errorLogContainterError'><p>This size currently out of stock.</p></div>" ).insertAfter( "#451_btn_orderadd" );
-			                
-			                function deleteContainer(){
-                              $( ".errorLogContainterError" ).remove();
-                            }
-                            setTimeout(deleteContainer, 3000);
-			                
-			            }
+                        }
+                        $( document ).on( "click", function() {
+                            $(  "div.errorLogContainterError"  ).remove();
+                        });
 			        }
 	            })();
 	        $injector.get('$rootScope').$broadcast('exception', ex, cause);
